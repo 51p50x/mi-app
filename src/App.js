af804e2dd6ccdc57818ad089f21d61434fabfa72
51p50x <51plog50@gmail.com>
2022-03-13 12:29:13 -0600

    Added Button Component
     * Added button class component and attached it to App Component just to see how behaves when rendering App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import {Component} from 'react'
 
 class Button extends Component {
-    
+    render(){
+        console.log('executing render method for button component');
+        return(
+            <button>Enviar</button>
+        )
+    }
 }
 
 class App extends Component{
@@ -11,11 +16,12 @@ class App extends Component{
         return(
             <div>
                 <p>Hola mundo</p>
+                <Button/>
                 <button 
                     className={`${this.state.valor}`} 
                     onClick={()=> this.setState({valor: 2})}
                 >
-                    Enviar
+                    Enviar en App
                 </button>
                 <p>{this.state.valor}</p>
             </div>
