bc3210eb7547d90d8d3b8f039bd4b9df37491573
51p50x <51plog50@gmail.com>
2022-03-13 19:40:57 -0600

    Added lifting up state logic in app.js file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
 import {Component} from 'react'
 
-// constructor -> render -> componentDidMount
-class Button extends Component {
-    state = {}
+class Input extends Component{
     
-    constructor(props){
-        super(props)
-        console.log('constructor', props);
-    }
-
     render(){
-        console.log('executing render method for button component');
         return(
-            <button onClick={()=>this.setState({prop: 1})}>
-                Enviar
-            </button>
+            <input
+                value={this.props.value}
+                onChange={this.props.onChange} 
+            />
+
         )
     }
-
-    componentDidMount(){
-        console.log('componentDidMount');
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        console.log('component did update', prevProps, prevState);
-    }
-
-    componentWillUnmount(){
-        console.log('unmounting component', this.props, this.state);
-    }
 }
-
 class App extends Component{
-    state = {valor:3}
+    state = {
+        nombre: '',
+        apellido: '',
+    }
+
+    updateValues = (prop, value) =>{
+        this.setState({[prop]:value})
+    }
+
     render(){
-        console.log(this.state);
         return(
-            <div>
-                <p>Hola mundo</p>
-                {this.state===3 ?
-                <Button waifu="happy" /> : null}
-                <button 
-                    className={`${this.state.valor}`} 
-                    onClick={()=> this.setState({valor: 2})}
-                >
-                    Enviar en App
-                </button>
-                <p>{this.state.valor}</p>
-            </div>
+            <p>
+                Nombre Completo: {`${this.state.nombre} ${this.state.apellido}`}
+                <Input 
+                    value = {this.state.nombre}
+                    onChange = {e => this.updateValues('nombre',e.target.value)}
+                />
+                <Input 
+                    value = {this.state.apellido} 
+                    onChange = {e => this.updateValues('apellido',e.target.value)}
+                />
+            </p>
         )
     }
 }
